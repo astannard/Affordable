4bbe30e8358abfa170a1c32f184cc635b1a6383a
Andrew Stannard <stannarda@gmail.com>
2021-11-21 17:10:32 +0000

    property finder  console project

---
+++ AffordableCalcTest/AffordabilityCalcTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Affordable;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AffordableCalcTest
+{
+    [TestClass]
+    public class AffordabilityCalcTests
+    {
+        [TestMethod]
+        public void AfforableTest()
+        {
+            var summary = new StatementRecurringSummary
+            {
+                TotalExpenses = 535.00m,
+                TotalIncome = 5000.00m
+            };
+
+            var affordable = AffordabilityCalc.CanAfford(summary, 600.00m);
+            Assert.AreEqual(true, affordable);
+        }
+
+        [TestMethod]
+        public void UnafforableTest()
+        {
+            var summary = new StatementRecurringSummary
+            {
+                TotalExpenses = 535.00m,
+                TotalIncome = 1000.00m
+            };
+
+            var affordable = AffordabilityCalc.CanAfford(summary, 800.00m);
+            Assert.AreEqual(false, affordable);
+        }
+
+
+    }
+}
---
+++ PropertyFinder/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using Affordable;
+using CsvHelper;
+
+namespace PropertyFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bankstatement = GetBankStatement();
+            var properties = GetProperties();
+            //So this bank statement tool is basic, It should remove current rental income from the calculation
+            //as they wont be paying rent twice. The CSV tool does not work and would need to be done differently with error handling for the import 
+            var found = Affordable.PropertyFinder.FindProperties(bankstatement, properties);
+            foreach(var prop in found)
+            {
+                Console.WriteLine(prop.Address);
+            }
+
+        }
+
+        private static List<BankStatementItem> GetBankStatement()
+        {
+            using (var reader = new StreamReader("../../../Files/bank_statement.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<BankStatementItem>();
+                return records.ToList();
+            }
+        }
+
+        private static List<Property> GetProperties()
+        {
+            using (var reader = new StreamReader("../../..Files/properties.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<Property>();
+                return records.ToList();
+            }
+        }
+    }
+}
